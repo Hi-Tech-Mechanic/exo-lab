62cf2463c76e4d2473205a1da061b7bfc2b9c426
Ruslan <ruslan.minatullaev-1000@yandex.ru>
2025-11-09 22:56:05 +0300

    Add constants parts (scales, tags)

--- Assets/PersonalAssets/Scripts/Constants/Scales.cs
+++ Assets/PersonalAssets/Scripts/Constants/Scales.cs
@@ -1,9 +1,13 @@
 namespace ExoLab.Constants
 {
+    using UnityEngine;
+
     public partial class Constants
     {
         public class Scales
         {
+            public static Vector2 ScaleVector2_110Percent = new Vector2(1.1F, 1.1F);
+
             public const float ScaleMultiplier_110Percent = 1.1F;
             public const float ScaleMultiplier_150Percent = 1.5F;
         }
---
+++ Assets/PersonalAssets/Scripts/Constants/Tags.cs
@@ -0,0 +1,10 @@
+namespace ExoLab.Constants
+{
+    public partial class Constants
+    {
+        public class Tags
+        {
+            public const string AudioSourceFromCanvas = "AudioSourceFromCanvas";
+        }
+    }
+}
