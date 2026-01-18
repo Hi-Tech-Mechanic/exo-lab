8e3cccb316766b4c61f623957d3d1b64b453a564
Ruslan <ruslan.minatullaev-1000@yandex.ru>
2026-01-18 10:32:48 +0300

    Доработка временного InputControl

--- Assets/TempInputControl.cs
+++ Assets/TempInputControl.cs
@@ -16,13 +16,12 @@ namespace ExoLab.Input
         [SerializeField] private Camera _cameraBack;
         [SerializeField] private Camera _cameraForward;
 
-        [SerializeField] private List<SuitComponentAbstract<SuitComponentItemData>> suitComponents;
-        [SerializeField] private Transform t;
+        [SerializeField] private List<Transform> suitComponents;
 
         [SerializeField] private GameObject AssemblyParent;
 
-        public List<Vector3> targetPosition = new();
-        public List<Quaternion> targetEulerAngles = new();
+        private List<Vector3> targetPosition = new();
+        private List<Quaternion> targetEulerAngles = new();
         private List<Transform> parentsTransforms = new();
 
         public GameObject inventory;
@@ -34,6 +33,13 @@ namespace ExoLab.Input
         private void Awake()
         {
             UpdateKeyBindings();
+
+            foreach (var e in suitComponents)
+            {
+                targetPosition.Add(e.transform.localPosition);
+                targetEulerAngles.Add(e.transform.localRotation);
+                parentsTransforms.Add(e.transform.parent);
+            }
         }
 
         // По событию изменения настроек клавиш будет обновляться привязки
@@ -63,24 +69,29 @@ namespace ExoLab.Input
         {
             if (Input.GetKeyDown(KeyCode.Alpha1))
             {
+                this.transform.GetChild(0).gameObject.SetActive(true);
+                AssemblyParent.SetActive(false);
                 _cameraBack.gameObject.SetActive(true);
                 _cameraForward.gameObject.SetActive(false);
             }
             if (Input.GetKeyDown(KeyCode.Alpha2))
             {
+                if (_cameraBack.gameObject.activeInHierarchy == false)
+                    return;
+
                 _cameraBack.gameObject.SetActive(false);
                 _cameraForward.gameObject.SetActive(true);
             }
             if (Input.GetKeyDown(KeyCode.Alpha5))
             {
-                //IEnumerator c = DescroySuit();
-                //StartCoroutine(c);
+                IEnumerator c = DescroySuit();
+                StartCoroutine(c);
                 Notifications.InvokeWarnNotify("Разрушение экзоскелета запущено", TransformDirections.RectDirection.Center);
             }
             if (Input.GetKeyDown(KeyCode.Alpha7))
             {
-                //IEnumerator c = RepairSuit();
-                //StartCoroutine(c);
+                IEnumerator c = RepairSuit();
+                StartCoroutine(c);
                 Notifications.InvokeStandardNotify("Регенерация экзоскелета запущена", TransformDirections.RectDirection.TopCenter);
             }
             if (Input.GetKeyDown(KeyCode.Alpha3))
@@ -93,7 +104,8 @@ namespace ExoLab.Input
             }
             if (Input.GetKeyDown(KeyCode.Alpha8))
             {
-                AssemblyParent.SetActive(!AssemblyParent.activeInHierarchy);
+                this.transform.GetChild(0).gameObject.SetActive(false);
+                AssemblyParent.SetActive(true);
             }
 
             if (Input.GetKeyDown(KeyCode.E))
@@ -104,21 +116,15 @@ namespace ExoLab.Input
             }
         }
 
-        //private void Awake()
-        //{
-        //    foreach (var e in suitComponents)
-        //    {
-        //        targetPosition.Add(e.transform.localPosition);
-        //        targetEulerAngles.Add(e.transform.localRotation);
-        //        parentsTransforms.Add(e.transform.parent);
-        //    }
-        //}
-
         IEnumerator DescroySuit()
         {
             foreach (var e in suitComponents)
             {
-                e.GetDamage(100, t);
+                //e.GetDamage(100, t);
+
+                e.SetParent(this.transform.parent);
+                e.GetComponent<Rigidbody>().isKinematic = false;
+                e.GetComponent<Rigidbody>().useGravity = true;
 
                 yield return new WaitForSeconds(0.5f);
             }
@@ -128,12 +134,13 @@ namespace ExoLab.Input
         {
             for (int i = 0; i < suitComponents.Count; i++)
             {
-                SuitComponentAbstract<SuitComponentItemData> e = suitComponents[i];
+                //SuitComponentAbstract<SuitComponentItemData> e = suitComponents[i];
+                var e = suitComponents[i];
 
+                e.transform.SetParent(parentsTransforms[i]);
                 e.transform.DOLocalMove(targetPosition[i], 0.7f);
                 e.transform.DOLocalRotateQuaternion(targetEulerAngles[i], 0.7f);
 
-                e.transform.SetParent(parentsTransforms[i]);
                 e.GetComponent<Rigidbody>().isKinematic = true;
                 e.GetComponent<Rigidbody>().useGravity = false;
 
